d6d7690e610eac72fda0fac499daab75516357c1
Aditya Jeswani <32266008+AdityaJ42@users.noreply.github.com>
2017-12-27 17:51:32 +0530

    Started the project along with the apps (#2)
    * Add files via upload
    
    * Add files via upload

---
+++ book_listing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookListingConfig(AppConfig):
+    name = 'book_listing'
---
+++ booke/sign_in/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SignInConfig(AppConfig):
+    name = 'sign_in'
