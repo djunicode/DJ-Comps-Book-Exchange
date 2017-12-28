28f0f23476e8358d5daab4e5864f041dfffd2769
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2017-12-28 19:55:40 +0530

    Add files via upload

--- sign_in/views.py
+++ sign_in/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from signup.forms import RegistrationForm 
+
 
 # Create your views here.
+
+def home(request):
+	return render(request, 'signup/home.html')
+
+def register(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/signup')
+		else:
+			args = {'form': form}
+			return render(request, 'signup/reg_form.html', args)
+	else:
+		form = RegistrationForm()
+
+		args = {'form': form}
+		return render(request, 'signup/reg_form.html', args)
