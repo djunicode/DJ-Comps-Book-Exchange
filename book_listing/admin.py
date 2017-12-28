a16b982ea6560843fc192b7046995f761f835ab7
Aditya Jeswani <adit98@gmail.com>
2017-12-28 19:33:45 +0530

    Created model for book listing

--- book_listing/admin.py
+++ book_listing/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Book_List
 
-# Register your models here.
+
+admin.site.register(Book_List)
--- book_listing/models.py
+++ book_listing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book_List(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    book_image = models.FileField(null=True)
