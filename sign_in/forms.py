f102af1623b82edc0177f6600dc3de99ef79cbab
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2017-12-28 19:52:34 +0530

    Add files via upload

---
+++ sign_in/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegistrationForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = {
+		'username',
+		'first_name',
+		'last_name',
+		'email',
+		'password1',
+		'password2'
+		}
+
+	def save(self,commit=True):
+		user = super(RegistrationForm, self).save(commit=False)
+		user.first_name = self.cleaned_data['first_name']
+		user.last_name = self.cleaned_data['last_name']
+		user.email = self.cleaned_data['email']
+
+		if commit:
+			user.save()
+
+		return user
