9c6e44e28097cde7b873fc53b9dd3b40462a8606
Aditya Jeswani <adit98@gmail.com>
2017-12-28 10:59:24 +0530

    Database update

--- booke/settings.py
+++ booke/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'booke.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booke',
+        'USER': 'postgres',
+        'PASSWORD': 'abc@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
