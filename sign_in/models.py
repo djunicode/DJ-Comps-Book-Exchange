1f4345861af49b2407ad4a96d0715adeb19b61ba
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2017-12-29 14:09:08 +0530

    Update models.py

--- sign_in/models.py
+++ sign_in/models.py
@@ -1,13 +1,19 @@
+"""."""
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+
 # Create your models here.
 class UserProfile(models.Model):
+	"""."""
+
 	user = models.OneToOneField(User)
 	sapid = models.IntegerField(default=0)
 
+
 def create_profile(sender, **kwargs):
+	"""."""
 	if kwargs['created']:
 		user_profile = UserProfile.objects.create(user=kwargs['instance'])
 
