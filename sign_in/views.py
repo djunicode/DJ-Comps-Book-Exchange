c812bf2977269ca2ed8202be390e70b6fa7c54ea
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2017-12-29 14:13:12 +0530

    Update views.py

--- sign_in/views.py
+++ sign_in/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, HttpResponse, redirect
-from signup.forms import RegistrationForm 
+"""."""
+from django.shortcuts import render, redirect
+from signup.forms import RegistrationForm
 
 
 # Create your views here.
 
 def home(request):
+	"""."""
 	return render(request, 'signup/home.html')
 
+
 def register(request):
+	"""."""
 	if request.method == 'POST':
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
