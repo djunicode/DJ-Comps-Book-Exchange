e4c9b50b839833361c5c8eede9b2b62494cfa238
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2017-12-29 14:12:40 +0530

    Update urls.py

--- sign_in/urls.py
+++ sign_in/urls.py
@@ -1,8 +1,9 @@
+"""."""
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login, logout
 urlpatterns = [
-	url(r'^$',views.home),
+	url(r'^$', views.home),
 	url(r'^login/$', login, {'template_name': 'signup/login.html'}),
 	url(r'^logout/$', logout, {'template_name': 'signup/logout.html'}),
 	url(r'^register/$', views.register, name='register'),
