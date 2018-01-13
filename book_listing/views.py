e8d22c3fe536d20663795c30e7f2c362b384ce7f
keval nagda <knagda008@gmail.com>
2018-01-14 01:17:48 +0530

    Update

--- book_listing/views.py
+++ book_listing/views.py
@@ -3,7 +3,8 @@ from .models import Book_List
 from .filters import BookFilter
 from django.urls import reverse_lazy
 from .forms import Book_ListForm
-from django.views.generic import (UpdateView, DeleteView)
+from django.views.generic import (UpdateView, DeleteView, CreateView)
+from django.views import generic
 
 
 def search(request):
@@ -21,5 +22,20 @@ class BookUpdateView(UpdateView):
 
 class BookDeleteView(DeleteView):
     model = Book_List
+    context_object_name = "item"
     template_name = "book_listing/delete_view.html"
-    success_url = reverse_lazy("books:search")
+    success_url = reverse_lazy("books:index")
+
+
+class IndexView(generic.ListView):
+    template_name = 'book_listing/index.html'
+    context_object_name = 'all_books'
+
+    def get_queryset(self):
+        return Book_List.objects.all()
+
+
+class BookCreate(CreateView):
+    model = Book_List
+    fields = ['uploaded_by', 'author', 'title', 'description', 'book_image', 'publication', 'semester', 'subject']
+
