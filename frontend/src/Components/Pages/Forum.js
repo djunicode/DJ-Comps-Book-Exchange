7b8e0f30f386a1df5cb885b390a4c919f588e897
Maulik Shah <23664526+MaulikSS@users.noreply.github.com>
2018-01-14 19:35:57 +0530

    Basic Template

---
+++ frontend/src/Components/Pages/Forum.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+class Forum extends Component {
+  render() {
+    return (
+      <div>
+          <Container>
+            <h1 style={{textAlign: 'center'}}>Forum Page</h1>
+          </Container>
+      </div>
+    );
+  }
+}
+
+export default Forum;
