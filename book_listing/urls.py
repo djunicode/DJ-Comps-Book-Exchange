ff90a9c50dedd6ac5d001129ae7532c654e3d3bb
keval nagda <knagda008@gmail.com>
2018-01-15 21:08:18 +0530

    Details

--- book_listing/urls.py
+++ book_listing/urls.py
@@ -5,7 +5,7 @@ app_name = "book_listing"
 
 urlpatterns = [
     url(r'^search/$', views.search, name='search'),
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^details/$', views.lender_details, name='details'),
     url(r'^add/$', views.BookCreate.as_view(), name='book-add'),
     url(r'^update/(?P<pk>\d+)/$', views.BookUpdateView.as_view(), name="update"),
     url(r'^delete/(?P<pk>\d+)/$', views.BookDeleteView.as_view(), name="delete"),
